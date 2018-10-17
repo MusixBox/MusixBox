a5b0ea20bd4e59f81715e29bac1f57c3d021ee50
Victor Jiao <VjiaoBlack@gmail.com>
2018-10-16 18:06:18 -0700

    added perlin noise gen

--- index.js
+++ index.js
@@ -2,52 +2,42 @@ var synth = new Tone.AMSynth().toMaster()
 
 //attach a listener to all of the buttons
 document.querySelectorAll('button').forEach(function(button){
-	button.addEventListener('mousedown', function(e){
-		//play the note on mouse down
-		synth.triggerAttack(e.target.textContent)
-	})
-	button.addEventListener('mouseup', function(e){
-		//release on mouseup
-		synth.triggerRelease()
-	})
+  button.addEventListener('mousedown', function(e){
+    //play the note on mouse down
+    synth.triggerAttack(e.target.textContent)
+  })
+  button.addEventListener('mouseup', function(e){
+    //release on mouseup
+    synth.triggerRelease()
+  })
 })
 
-var canvas = document.getElementsByTagName('canvas')[0];
-canvas.width = 1024;
-canvas.height = 768;
-var ctx = canvas.getContext('2d');
-var image = ctx.createImageData(canvas.width, canvas.height);
-var data = image.data;
-var start = Date.now();
-for (var x = 0; x < canvas.width; x++) {
-  if (x % 100 == 0) {
-   noise.seed(Math.random());
-  }
-  for (var y = 0; y < canvas.height; y++) {
-    var value = Math.abs(noise.perlin2(x / 100, y / 100));
-    value *= 256;
-    var cell = (x + y * canvas.width) * 4;
-    data[cell] = data[cell + 1] = data[cell + 2] = value;
-    data[cell] += Math.max(0, (25 - value) * 8);
-    data[cell + 3] = 255; // alpha.
-  }
+// noise.seed(Math.random());
+noise.seed(0);
+
+var value = 0,
+lastFrameTimeMs = 0,
+maxFPS=60;
+
+ 
+function update(delta, timestamp) { 
+  value = (noise.perlin2(timestamp / 1000, 0) + 0.5) * 256;
 }
-/* // Benchmark code.
-start = Date.now();
-for (var x = 0; x < 10000; x++) {
-  for (var y = 0; y < 10000; y++) {
-    noise.simplex2(x / 50, y/50);
+ 
+function mainLoop(timestamp) {
+  if (timestamp < lastFrameTimeMs + (1000 / maxFPS)) {
+    requestAnimationFrame(mainLoop);
+    return;
   }
-}*/
-var end = Date.now();
-ctx.fillColor = 'black';
-ctx.fillRect(0, 0, 100, 100);
-ctx.putImageData(image, 0, 0);
-ctx.font = '16px sans-serif'
-ctx.textAlign = 'center';
-ctx.fillText('Rendered in ' + (end - start) + ' ms', canvas.width / 2, canvas.height - 20);
-if(console) {
-  console.log('Rendered in ' + (end - start) + ' ms');
+  lastFrameTimeMs = timestamp;
+  delta = timestamp - lastFrameTimeMs; // get the delta time since last frame
+  lastFrameTimeMs = timestamp;
+ 
+  update(delta, timestamp); // pass delta to update
+  
+  
+  document.body.style.backgroundColor = "rgb(" + value + "," + value + "," + value + ")";
+  requestAnimationFrame(mainLoop);
 }
 
-
+requestAnimationFrame(mainLoop);
