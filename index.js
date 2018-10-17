7fbce4e4de3ae3ea996eeb648f023b598cc38af7
Victor Jiao <VjiaoBlack@gmail.com>
2018-10-16 19:41:34 -0500

    add example from tone.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-////create a synth and connect it to the master output (your speakers)create a s 
-var synth = new Tone.Synth().toMaster();
+var synth = new Tone.AMSynth().toMaster()
 
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
+//attach a listener to all of the buttons
+document.querySelectorAll('button').forEach(function(button){
+	button.addEventListener('mousedown', function(e){
+		//play the note on mouse down
+		synth.triggerAttack(e.target.textContent)
+	})
+	button.addEventListener('mouseup', function(e){
+		//release on mouseup
+		synth.triggerRelease()
+	})
+})
