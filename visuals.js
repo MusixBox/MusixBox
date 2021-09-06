e8ad5ad7679d084fc9fd720869ffddac4e0e6aa5
Tye Hadfield <tye_hadfield@hotmail.com>
2021-09-06 15:05:19 +1000

    Was able to get rid of the side panels with changing some of te perspectives for the camera.

--- visuals.js
+++ visuals.js
@@ -20,7 +20,7 @@ var bkndColor = new THREE.Color('#000000');
 
 // Create basic scene
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 5 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
